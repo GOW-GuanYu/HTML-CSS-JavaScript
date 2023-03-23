e5d0245fcac430c3d5613cd21bb40eed60297348
Arthur Yvens <arthuryvens85@gmail.com>
2023-03-23 16:52:31 -0300

    atualização
    Atualizei um exercicio de JS

--- JavaScript/fonte.js
+++ JavaScript/fonte.js
@@ -1,1 +1,20 @@
 console.log("Olá, Mundo!")
+
+
+//Declarando Váriaveis
+var preco = 10;
+var desconto = 0.2;
+
+// Descontos
+
+var total0 = (preco * (desconto));// Desconto de 80%
+var total1 = (preco * (1- desconto));// Desconto de 20%
+var total2 = (10 * 0.8); // Desconto de 20%
+var total3 = (10 * 0.2);// Desconto de 80%
+
+
+//Mostrando resultados
+console.log(total0)
+console.log(total1)
+console.log(total2)
+console.log(total3)
