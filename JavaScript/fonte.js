9a8157e3ba892426259b86d4ccfb813e8cab2f7e
Arthur Yvens <arthuryvens85@gmail.com>
2023-03-23 17:09:58 -0300

    atualização
    adicionei algumas novas atualizações

--- JavaScript/fonte.js
+++ JavaScript/fonte.js
@@ -1,20 +1,39 @@
 console.log("Olá, Mundo!")
 
 
-//Declarando Váriaveis
-var preco = 10;
-var desconto = 0.2;
+// Declarando Variáveis  
+// Let = Forma mais usual de criar uma variável
+
+let preco = 10;
+let desconto = 0.2;
 
 // Descontos
 
 var total0 = (preco * (desconto));// Desconto de 80%
 var total1 = (preco * (1- desconto));// Desconto de 20%
+
 var total2 = (10 * 0.8); // Desconto de 20%
 var total3 = (10 * 0.2);// Desconto de 80%
 
+let nome = "Caderno"; // String (texto) -> Sequência de letras/simbolos
+let categoria = "Papelaria";
 
-//Mostrando resultados
-console.log(total0)
-console.log(total1)
-console.log(total2)
-console.log(total3)
+// Mostrando Resultados
+console.log(total0);
+console.log(total1);
+console.log(total2);
+console.log(total3);
+
+
+console.log(nome + categoria);
+
+console.log("Produto: " + nome 
+    + ", Categoria: " + categoria
+    + ", Preço: " + preco
+    + ", Preco com Desconto de 20%: " + total1   
+    );
+
+//exemplo de impressão de stings (texto)
+
+console.log(1+1)// Não é String
+console.log("1"+"1");// String
