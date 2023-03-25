1d551de42c5b12a3778ec6622f8e1fe5a1046d24
Arthur Yvens <arthuryvens85@gmail.com>
2023-03-25 14:34:48 -0300

    atualização
    adicionei um novo arquivo

---
+++ JavaScript/fonte0.js
@@ -0,0 +1,21 @@
+console.log("Olá, Mundo!")
+
+// Declarando váriaveis
+
+var nome = "Caneta"; // Necessário o uso de aspas
+var quantidade = 10;
+var preco = 6.5;
+var imposto = 1.5;
+var precofinal = preco + imposto;
+
+// Não esquecer de utilizar o ; assim como no C++ e no CSS
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precofinal);
+
+// Modificando váriavel
+nome = "Caneta Compacta Azul";
+console.log(nome);
