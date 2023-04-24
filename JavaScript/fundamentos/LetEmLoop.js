e12a2363bae1e915c1a15cbe8c2dc711d9459109
Arthur Yvens <arthuryvens85@gmail.com>
2023-04-24 16:34:27 -0300

    commit
    .

---
+++ JavaScript/fundamentos/LetEmLoop.js
@@ -0,0 +1,12 @@
+for (let i = 1; i < 11; i++){
+
+    console.log('i =', i)
+
+    if(i == 5){
+
+        console.log('i é igual a 5')
+
+    }
+    else{}
+
+}
---
+++ JavaScript/fundamentos/VarEmLoop.js
@@ -0,0 +1,7 @@
+const funcs = [] // array vazio
+
+for(var i = 0; i < 10; i++){
+
+   funcs.push() //adicioonando novo alemento ao array
+
+}
