613b0c4db4975783b6fd01d8926126e31b9b18c6
Arthur Yvens <arthuryvens85@gmail.com>
2023-04-24 16:47:25 -0300

    commit
    .

--- JavaScript/fundamentos/LetEmLoop2.js
+++ JavaScript/fundamentos/LetEmLoop2.js
@@ -2,12 +2,13 @@ const funcs = [] // array vazio
 
 for(let i = 0; i < 10; i++){
 
-   funcs.push(function() { //adicionando novo function(){} ao array 
+   funcs.push(function() { // adicionando novo function(){} ao array 
         console.log(i)
     }) 
 
 }
 
-//adicionando quais são os novos elementos adicionados
+// adicionando quais são os novos elementos
 funcs[2]()
+funcs[6]()
 funcs[8]()
---
+++ JavaScript/hoisting.js
@@ -0,0 +1,11 @@
+// por isso é sempre importante começar declarando as váriaveis
+console.log('a =', a)
+var a = 2
+console.log('a =', a)
+
+function teste(){
+    console.log('b =', b)
+    var b = 2
+    console.log('b =', b)
+}
+teste()
