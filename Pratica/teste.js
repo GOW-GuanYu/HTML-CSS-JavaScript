b62d391958cfe7578d735f16868ce560ec8783fe
Arthur Yvens <arthuryvens85@gmail.com>
2023-06-29 17:30:06 -0300

    commit
    Um pequeno teste para entender melhor os CASES e os IF e ELSE IF

---
+++ Pratica/teste.js
@@ -0,0 +1,28 @@
+const imprimirNota = function (nota){
+    switch (Math.floor(nota)){
+        case 10:
+            console.log('Parabéns! você atingiu a nota máxima em excelência.') 
+        break
+        case 9: 
+            console.log('Parabéns! Você ficou por um triz de conquistar a nota máxima, continue se esforçando. ;)')
+        break    
+        case 8: case 7:
+            console.log('Você passou no teste!')
+        break
+        case 6: case 5:
+            console.log('Faça o teste de recuperação de notas.')
+        break
+        case 4: case 3: case 2: case 1: case 0:
+            console.log('Você falhou no teste! Boa sorte na próxima vez. :)')
+        break
+}
+if (nota == 10){
+    console.log('Você está legivel para o sorteio de um carro zero! entre em contato com a diretoria para melhor detalhes.')
+}
+else if (nota == 9){
+    console.log('Você está legivel para o sorteio de um carro zero! entre em contato com a diretoria para melhor detalhes.')
+}
+else{}
+
+}
+imprimirNota(9)
