09ab04a753253e3a241a91fd581d7af64492b247
Arthur Yvens <arthuryvens85@gmail.com>
2023-07-04 02:47:26 -0300

    coommit
    .

---
+++ JavaScript/controle/teste3.js
@@ -0,0 +1,9 @@
+let numero1 = 2
+let numero2 = 2
+const teste = (soma) =>{
+    soma = numero1 + numero2
+    console.log('A soma entre os dois números deu:' + soma)
+}
+teste(0)
+// testando se consigo implementar a soma de dois valores fora da função arrow. (sim, consigo)
+
