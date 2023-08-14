2bad7bd232ccd83261b61fb23ce512ad23478777
Arthur Yvens <arthuryvens85@gmail.com>
2023-08-14 17:05:14 -0300

    commit
    .

--- JavaScript/funcao/callBack3.js
+++ JavaScript/funcao/callBack3.js
@@ -1,5 +1,5 @@
 // Exemplo de callback no browser (coloque no console do browser)
-document.getElementsByTagName('body')[0].onclick = function(f){  // atribuindo uma função ao click do elemento "body" 
+document.getElementsByTagName('body')[0].onclick = function(f){  /// atribuindo uma função ao click do elemento "body" 
     // o [0] representa o primeiro indice/elemento, vc precisa acessar o elemento especifico para a função...funcionar
     console.log('O evento ocorreu!')
 }
